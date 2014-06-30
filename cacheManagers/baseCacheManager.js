732a4ccb1106bc1b47ce421536d6160d05cab4ba
swaraj <swaraj@expectlabs.com>
2014-06-30 13:30:41 -0700

    install npm deps

--- cacheManagers/baseCacheManager.js
+++ cacheManagers/baseCacheManager.js
@@ -2,6 +2,7 @@ var fs = require('fs');
 var path = require('path');
 var logger = require('../util/logger');
 var md5 = require('MD5');
+var shell = require('shelljs');
 
 function BaseCacheManager (cacheDirectory) {
   this.cacheDirectory = cacheDirectory;
@@ -12,18 +13,29 @@ var getFileHash = function (filePath) {
   return md5(file);
 };
 
-BaseCacheManager.prototype.name = 'BaseCacheManager';
+BaseCacheManager.prototype.getName = function () {
+  return 'BaseCacheManager';
+};
+
+BaseCacheManager.prototype.getCliName = function () {
+  logger.logError('Override getCliName() in subclasses!');
+};
 
 BaseCacheManager.prototype.getConfigPath = function () {
   logger.logError('Override getConfigPath() in subclasses!');
 };
 
+BaseCacheManager.prototype.getInstalledDirectory = function () {
+  logger.logError('Override getInstalledDirectory() in subclasses!');
+};
+
+
 BaseCacheManager.prototype.cacheLogInfo = function (message) {
-  logger.logInfo('[' + this.name + '] ' + message);
+  logger.logInfo('[' + this.getName() + '] ' + message);
 };
 
 BaseCacheManager.prototype.cacheLogError = function (error) {
-  logger.logError('[' + this.name + '] ' + error);
+  logger.logError('[' + this.getName() + '] ' + error);
 };
 
 BaseCacheManager.prototype.cacheExists = function (hash) {
@@ -33,6 +45,10 @@ BaseCacheManager.prototype.cacheExists = function (hash) {
 
 BaseCacheManager.prototype.installDependencies = function () {
   logger.logError('Override installDependencies() in subclasses!');
+};
+
+BaseCacheManager.prototype.archiveDependencies = function (hash) {
+  var installedDirectory = path.resolve(process.cwd(), this.getInstalledDirectory());
 };
 
 BaseCacheManager.prototype.loadDependencies = function () {
@@ -51,9 +67,13 @@ BaseCacheManager.prototype.loadDependencies = function () {
   if (this.cacheExists(hash)) {
     console.log('cache exists');
     // install from cache
-  }
-  else {
+  } else { // install dependencies with CLI tool
+    if (! shell.which(this.getCliName())) {
+      this.cacheLogError('Command line tool ' + this.getCliName() + ' not installed');
+      return;
+    }
     this.installDependencies();
+    this.archiveDependencies();
     // then cache
   }
 
--- cacheManagers/npmCacheManager.js
+++ cacheManagers/npmCacheManager.js
@@ -1,5 +1,6 @@
 var BaseCacheManager = require('./baseCacheManager');
 var path = require('path');
+var shell = require('shelljs');
 
 // Inherit from BaseCacheManager
 function NpmCacheManager (cacheDirectory) {
@@ -9,14 +10,31 @@ NpmCacheManager.prototype = Object.create(BaseCacheManager.prototype);
 NpmCacheManager.prototype.constructor = NpmCacheManager;
 
 // Override BaseCacheManager methods
-NpmCacheManager.prototype.name = 'npm';
+NpmCacheManager.prototype.getName = function () {
+  return 'npm';
+};
 
 NpmCacheManager.prototype.getConfigPath = function () {
   return path.resolve(process.cwd(), 'package.json');
 };
 
+NpmCacheManager.prototype.getCliName = function () {
+  return 'npm';
+};
+
+NpmCacheManager.prototype.getInstalledDirectory = function () {
+  return 'node_modules';
+};
+
 NpmCacheManager.prototype.installDependencies = function () {
-  this.cacheLogInfo('about to install das dependencies');
+  this.cacheLogInfo('about to install npm dependencies');
+  if (shell.exec('npm install').code !== 0) {
+    this.cacheLogError('error running npm install');
+    return;
+  }
+  this.cacheLogInfo('installed npm dependencies, now archiving');
+
+
 };
 
 
