41952da996f21ac7892ad5e026d73232d781d060
swaraj <swaraj@expectlabs.com>
2014-07-05 16:50:10 -0700

    replaced yargs with nomnom for better command parsing

--- index.js
+++ index.js
@@ -4,69 +4,72 @@
 var fs = require('fs');
 var path = require('path');
 var shell = require('shelljs');
+var parser = require('nomnom');
 
 var util = require('./util/util');
 var logger = require('./util/logger');
-var CacheDependencyManager = require('./cacheDependencyManagers/baseCacheDependencyManager');
-
 
 if (! shell.which('tar')) {
   logger.logError('tar command-line tool not found. exiting...');
   return;
 }
 
-// Parse command line options
-var yargs = require('yargs')
-  .usage('Usage: $0 install')
-  .example('$0 install', 'try to install npm, bower, and composer components')
-  .example('$0 install bower', 'install only bower components')
-  .example('$0 install bower npm', 'install bower and npm components')
-  .example('$0 install --cacheDirectory /Users/cached/', 'try to install npm, bower, and composer components, using /Users/cached/ as cache directory')
-  .alias('h', 'help')
-  .string('cacheDirectory');
-var argv = yargs.argv;
+// npm-cache command handlers
 
-var shouldInstall = argv._.indexOf('install') !== -1;
+// main method for installing specified dependencies
+var installDependencies = function (opts) {
+  logger.logInfo('using ' + opts.cacheDirectory + ' as cache directory');
+  if (! fs.existsSync(opts.cacheDirectory)) {
+    // create directory if it doesn't exist
+    shell.mkdir('-p', opts.cacheDirectory);
+    logger.logInfo('creating cache directory');
+  }
 
-// Show help message if help flag specified
-// or install not specified
-if (argv.help || ! shouldInstall) {
-  console.log(yargs.help());
-  return;
-}
+  var availableManagers = {
+    npm: './cacheDependencyManagers/npmConfig.js',
+    bower: './cacheDependencyManagers/bowerConfig.js',
+    composer: './cacheDependencyManagers/composerConfig.js'
+  };
+  var defaultManagers = Object.keys(availableManagers);
 
-// Use ~/.package_cache as default cacheDirectory, or use user-specified directory
-var cacheDirectory = argv.cacheDirectory || path.resolve(util.getHomeDirectory(), '.package_cache');
-logger.logInfo('using ' + cacheDirectory + ' as cache directory');
-if (! fs.existsSync(cacheDirectory)) {
-  // create directory if it doesn't exist
-  shell.mkdir('-p', cacheDirectory);
-  logger.logInfo('creating cache directory');
-}
-
-
-var availableManagers = {
-  npm: './cacheDependencyManagers/npmConfig.js',
-  bower: './cacheDependencyManagers/bowerConfig.js',
-  composer: './cacheDependencyManagers/composerConfig.js'
+  // Parse args for which dependency managers to install
+  var specifiedManagers;
+  if (opts._.length === 1) { // no managers specified, install everything!
+    specifiedManagers = defaultManagers;
+  } else {
+    specifiedManagers = opts._;
+  }
+  var CacheDependencyManager = require('./cacheDependencyManagers/baseCacheDependencyManager');
+  specifiedManagers.forEach(function (dependencyManagerName) {
+    if (dependencyManagerName in availableManagers) {
+      logger.logInfo('installing ' + dependencyManagerName + ' dependencies');
+      var config = require(availableManagers[dependencyManagerName]);
+      var manager = new CacheDependencyManager(config);
+      manager.loadDependencies(opts.cacheDirectory);
+    }
+  });
 };
 
-var defaultManagers = ['npm', 'bower', 'composer'];
 
-// Parse args for which dependency managers to install
-var specifiedManagers;
-if (argv._.length === 1) {
-  specifiedManagers = defaultManagers;
-} else {
-  specifiedManagers = argv._;
-}
 
-specifiedManagers.forEach(function (dependencyManagerName) {
-  if (dependencyManagerName in availableManagers) {
-    logger.logInfo('installing ' + dependencyManagerName + ' dependencies');
-    var config = require(availableManagers[dependencyManagerName]);
-    var manager = new CacheDependencyManager(config);
-    manager.loadDependencies(cacheDirectory);
-  }
-});
+parser.command('install')
+  .callback(installDependencies)
+  .option('cacheDirectory', {
+    default: path.resolve(util.getHomeDirectory(), '.package_cache'),
+    abbr: 'c',
+    help: 'directory where dependencies will be cached'
+  })
+  .help('install specified dependencies');
 
+
+var examples = [
+  'Examples:',
+  '\tnpm-cache install\t# try to install npm, bower, and composer components',
+  '\tnpm-cache install bower\t# install only bower components',
+  '\tnpm-cache install bower npm\t# install bower and npm components',
+  '\tnpm-cache install bower --cacheDirectory /home/cache/\t# install bower components using /home/cache as cache directory'
+];
+
+parser.help(examples.join('\n'));
+
+parser.parse();
