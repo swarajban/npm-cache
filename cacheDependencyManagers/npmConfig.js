4dc3e2845d4fc157c5a42e4edbbb32124a5d55f5
swaraj <swaraj@expectlabs.com>
2015-03-21 14:02:13 -0700

    added support for npm-shrinkwrap.json

--- cacheDependencyManagers/npmConfig.js
+++ cacheDependencyManagers/npmConfig.js
@@ -1,10 +1,28 @@
 'use strict';
 
 var path = require('path');
+var fs = require('fs');
+var logger = require('../util/logger');
+
+
+// Returns path to configuration file for npm. Uses
+// npm-shrinkwrap.json if it exists; otherwise,
+// defaults to package.json
+var getNpmConfigPath = function () {
+  var shrinkWrapPath = path.resolve(process.cwd(), 'npm-shrinkwrap.json');
+  var packagePath = path.resolve(process.cwd(), 'package.json');
+  if (fs.existsSync(shrinkWrapPath)) {
+    logger.logInfo('[npm] using npm-shrinkwrap.json instead of package.json');
+    return shrinkWrapPath;
+  } else {
+    return packagePath;
+  }
+};
+
 
 module.exports = {
   cliName: 'npm',
-  configPath: path.resolve(process.cwd(), 'package.json'),
+  configPath: getNpmConfigPath(),
   installDirectory: 'node_modules',
   installCommand: 'npm install'
 };
