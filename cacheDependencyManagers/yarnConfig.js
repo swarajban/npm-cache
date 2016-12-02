b9441e103e82abfc7d1a0441a9300383568ef57e
Guillaume Maron <guillaume.maron@dashlane.com>
2016-12-02 15:48:21 +0100

    using yarn.lock if it exists, package.json if yarn.lock doesn't exist

--- cacheDependencyManagers/yarnConfig.js
+++ cacheDependencyManagers/yarnConfig.js
@@ -5,17 +5,38 @@ var shell = require('shelljs');
 var fs = require('fs');
 var md5 = require('md5');
 var logger = require('../util/logger');
+var isUsingYarnLock = null;
 
 
 // Returns path to configuration file for yarn. Uses
 // yarn.lock
 var getYarnConfigPath = function () {
-  return path.resolve(process.cwd(), 'yarn.lock');
+  var yarnLockPath = path.resolve(process.cwd(), 'yarn.lock');
+  var npmJsonPath = path.resolve(process.cwd(), 'package.json');
+
+  if (isUsingYarnLock === null) {
+      if (fs.existsSync(yarnLockPath)) {
+          logger.logInfo('[yarn] using yarn.lock instead of composer.json');
+          isUsingYarnLock = true;
+      }  else {
+          isUsingYarnLock = false;
+      }
+  }
+
+  return isUsingYarnLock ? yarnLockPath : npmJsonPath;
 };
 
 function getFileHash(filePath) {
-  var yarnlockfile = fs.readFileSync(filePath);
-  return md5(yarnlockfile);
+  if (isUsingYarnLock) {
+      var yarnlockfile = fs.readFileSync(filePath);
+      return md5(yarnlockfile);
+  } else {
+      var json = JSON.parse(fs.readFileSync(filePath));
+      return md5(JSON.stringify({
+          dependencies: json.dependencies,
+          devDependencies: json.devDependencies
+      }));
+  }
 }
 
 module.exports = {
