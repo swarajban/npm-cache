802c4ec3bfe5d54ff4a6c63ce388f50b95085dd5
Alexander von Weiss <alexvonweiss@googlemail.com>
2017-06-08 12:00:51 +0200

    add package-lock.json support introducted by npm@5

--- cacheDependencyManagers/npmConfig.js
+++ cacheDependencyManagers/npmConfig.js
@@ -8,17 +8,24 @@ var logger = require('../util/logger');
 
 
 // Returns path to configuration file for npm. Uses
-// npm-shrinkwrap.json if it exists; otherwise,
-// defaults to package.json
+// - npm-shrinkwrap.json if it exists; otherwise,
+// - package-lock.json if it exists; otherwise,
+// - defaults to package.json
 var getNpmConfigPath = function () {
   var shrinkWrapPath = path.resolve(process.cwd(), 'npm-shrinkwrap.json');
-  var packagePath = path.resolve(process.cwd(), 'package.json');
   if (fs.existsSync(shrinkWrapPath)) {
     logger.logInfo('[npm] using npm-shrinkwrap.json instead of package.json');
     return shrinkWrapPath;
-  } else {
-    return packagePath;
   }
+  
+  var packageLockPath = path.resolve(process.cwd(), 'package-lock.json');
+  if (fs.existsSync(packageLockPath)) {
+    logger.logInfo('[npm] using package-lock.json instead of package.json');
+    return packageLockPath;
+  }
+
+  var packagePath = path.resolve(process.cwd(), 'package.json');
+  return packagePath;
 };
 
 function getFileHash(filePath) {
